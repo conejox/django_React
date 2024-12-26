06fdf37cc1d7e02309f398a044febb38c18ba29e
conejox <albertoconejerosa@gmail.com>
2024-12-26 20:09:30 -0300

    package.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/Login"
+import Login from "./pages/login"
 import Register from "./pages/Register"
 import Home from "./pages/home"
 import NotFound from "./pages/NotFound"
